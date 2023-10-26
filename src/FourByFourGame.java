87414ca643c5e8698deb7673fc28dff765a17ca1
Anna Boronkai <annaboronki@hotmail.com>
2023-10-26 22:25:51 +0200

    replacing buttons 1-15 with array

--- src/FourByFourGame.java
+++ src/FourByFourGame.java
@@ -6,59 +6,28 @@ import java.awt.*;
 public class FourByFourGame extends JFrame {
 
     JPanel mainPanel = new JPanel();
-
-    JButton oneB = new JButton("1");
-    JButton twoB = new JButton("2");
-    JButton threeB = new JButton("3");
-    JButton fourB = new JButton("4");
-    JButton fiveB = new JButton("5");
-    JButton sixB = new JButton("6");
-    JButton sevenB = new JButton("7");
-    JButton eightB = new JButton("8");
-    JButton nineB = new JButton("9");
-    JButton tenB = new JButton("10");
-    JButton elevenB = new JButton("11");
-    JButton twelveB = new JButton("12");
-    JButton thirteenB = new JButton("13");
-    JButton fourteenB  = new JButton("14");
-    JButton fifteenB = new JButton("15");
+    JButton [] buttons = new JButton[15];
     JPanel emptySlot = new JPanel();
 
 
 public void game(){
 
-    this.add(mainPanel);
-    mainPanel.add(oneB);
-    mainPanel.add(twoB);
-    mainPanel.add(threeB);
-    mainPanel.add(fourB);
-    mainPanel.add(fiveB);
-    mainPanel.add(sixB);
-    mainPanel.add(sevenB);
-    mainPanel.add(eightB);
-    mainPanel.add(nineB);
-    mainPanel.add(tenB);
-    mainPanel.add(elevenB);
-    mainPanel.add(twelveB);
-    mainPanel.add(thirteenB);
-    mainPanel.add(fourteenB);
-    mainPanel.add(fifteenB);
-    mainPanel.add(emptySlot);
-
+    add(mainPanel);
     mainPanel.setLayout(new GridLayout(4, 4));
 
+    for(int i = 0; i < 15; i++) {
+        buttons[i] = new JButton(String.valueOf(i + 1));
+        mainPanel.add(buttons[i]);
+    }
 
+    mainPanel.add(emptySlot);
 
-
-    OurActionListener listener = new OurActionListener(oneB, twoB, threeB, fourB, fiveB, sixB, sevenB, eightB, nineB,
-            tenB, elevenB, twelveB, thirteenB, fourteenB, fifteenB, emptySlot);
-    oneB.addActionListener(listener);
+    OurActionListener listener = new OurActionListener(buttons, emptySlot);
 
     setVisible(true);
     setLocationRelativeTo(null);
     setSize(400,400);
     setDefaultCloseOperation(EXIT_ON_CLOSE);
-
 
 }
 
--- src/OurActionListener.java
+++ src/OurActionListener.java
@@ -4,48 +4,18 @@ import java.awt.event.ActionListener;
 
 public class OurActionListener implements ActionListener {
 
-    private JButton oneB;
-    private JButton twoB;
-    private JButton threeB;
-    private JButton fourB;
-    private JButton fiveB;
-    private JButton sixB;
-    private JButton sevenB;
-    private JButton eightB;
-    private JButton nineB;
-    private JButton tenB;
-    private JButton elevenB;
-    private JButton twelveB;
-    private JButton thirteenB;
-    private JButton fourteenB;
-    private JButton fifteenB;
+    private JButton[] buttons;
     private JPanel emptySlot;
 
-    public OurActionListener(JButton oneB, JButton twoB, JButton threeB, JButton fourB,JButton fiveB, JButton sixB,
-                             JButton sevenB, JButton eightB, JButton nineB, JButton tenB, JButton elevenB,
-                             JButton twelveB, JButton thirteenB, JButton fourteenB, JButton fifteenB, JPanel emptySlot){
-        this.oneB = oneB;
-        this.twoB = twoB;
-        this.threeB = threeB;
-        this.fourB = fourB;
-        this.fiveB = fiveB;
-        this.sixB = sixB;
-        this.sevenB = sevenB;
-        this.eightB = eightB;
-        this.nineB = nineB;
-        this.tenB = tenB;
-        this.elevenB = elevenB;
-        this.twelveB = twelveB;
-        this.thirteenB = thirteenB;
-        this.fourteenB = fourteenB;
-        this.fifteenB = fifteenB;
+    public OurActionListener(JButton[] buttons, JPanel emptySlot){
+        this.buttons = buttons;
         this.emptySlot = emptySlot;
 
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == oneB){
+        if(e.getSource() == buttons){
             System.out.println("Ok");
         }
     }
