0f757c39128b90c9461eadfad5ceb5426bac77c2
Stephanie <stephanie.zevian@yh.nackademin.se>
2023-10-26 13:41:09 +0200

    Added a comment

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
+        // I denna branch har vi våran rena kod
+
         System.out.println("Hello world!");
         System.out.println("Hej Anna");
     }
