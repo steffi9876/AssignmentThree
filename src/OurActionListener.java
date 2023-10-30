50110ed3310c118948ba3225be21322f21090eba
Stephanie <stephanie.zevian@yh.nackademin.se>
2023-10-30 16:27:28 +0100

    Started on actionperformed, might throw it!

--- src/OurActionListener.java
+++ src/OurActionListener.java
@@ -4,21 +4,34 @@ import java.awt.event.ActionListener;
 
 public class OurActionListener implements ActionListener {
 
-    private JButton[][] tile;
-    private JPanel emptySlot;
-
-    public OurActionListener(JButton[][] tile){
-        this.tile = tile;
-        this.emptySlot = emptySlot;
-
+    private JButton[][] tiles;
+    private GameLogic gameLogic;
+    public OurActionListener(JButton[][] tile, GameLogic gameLogic){
+        this.tiles = tile;
+        this.gameLogic = gameLogic;
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) { //ändrat nedan
-        if(e.getSource() == tile){
-           //Booleansk variabel som kollar om knappen kan flyttas, dvs om emptySlot finns över, under,
-           // eller vid sidan av knappen
-            //EV en toggler här?
+    public void actionPerformed(ActionEvent e) {
+       JButton clickedButton = (JButton) e.getSource();
+       int clickedRow = -1, clickedCol = -1;
+
+        for (int row = 0; row < 4; row++) {
+            for (int col = 0; col < 4; col++) {
+                if (tiles[row][col] == clickedButton) {
+                    clickedRow = row;
+                    clickedCol = col;
+                    break;
+                }
+            }
         }
-    }
-}
+        if (clickedRow != 1 && clickedCol != -1){
+            boolean moveable = gameLogic.isTileMoveable(clickedRow, clickedCol);
+            if (moveable){
+                gameLogic.swapTileWithEmptySlot(clickedRow, clickedCol);
+                //if (gameLogic.)
+                }
+            }
+        }
+        }
+
