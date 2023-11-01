615780c93cf98280a30a4ddd38f3becefa4f5dc3
Stephanie <stephanie.zevian@yh.nackademin.se>
2023-11-01 15:21:27 +0100

    Added comments to actionlistener klassen

--- src/OurActionListener.java
+++ src/OurActionListener.java
@@ -4,24 +4,25 @@ import java.awt.event.ActionListener;
 
 public class OurActionListener implements ActionListener {
 
-    private JButton[][] tile;
-    private GameLogic gameLogic;
+    private JButton[][] tile; // Våra spelbrickor
+    private GameLogic gameLogic; // En instans av gamelogic klassen
 
 
-    public OurActionListener(JButton[][] tile, GameLogic gameLogic) {
+    public OurActionListener(JButton[][] tile, GameLogic gameLogic) { // Konstruktor som tar emot spelbrickor och gameLogic-instansen
         this.tile = tile;
         this.gameLogic = gameLogic;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        JButton tile = (JButton) e.getSource();//Typomvandlar genom casting för att slippa en e.getSource för 15 knappar
+        JButton tile = (JButton) e.getSource(); // Hämtar den knapp (bricka) som utlöste händelsen och lagrar den i variabeln "tile"
         if(tile.getText().equals("")){ //om den tryckta knappen innehåller en tom sträng "" så gör inget
             return;
         }
         int clickedRow = -1;
         int clickedColumn = -1;
 
+        // Loopar igenom raderna och kolumnerna för att hitta den tryckta brickans position
         for (int row = 0; row < 4; row++) {
             for (int column = 0; column < 4; column++) {
 
@@ -32,11 +33,12 @@ public class OurActionListener implements ActionListener {
                 }
             }
         }
-        if (gameLogic.isTileMoveable(clickedRow, clickedColumn)){
-            gameLogic.swapTileWithEmptySlot(clickedRow,clickedColumn);
-            gameLogic.isTileEmptySetRed();
+        if (gameLogic.isTileMoveable(clickedRow, clickedColumn)){ // Kontrollera om den tryckta brickan kan flyttas med hjälp av spelets logik
+            gameLogic.swapTileWithEmptySlot(clickedRow,clickedColumn); // Om brickan kan flyttas, byt plats på brickan med den tomma brickan och uppdatera gränssnittet
+
+            gameLogic.isTileEmptySetRed(); // Kolla om den tomma brickan ska markeras som röd
         }
-        if (gameLogic.isGameSolved()){
+        if (gameLogic.isGameSolved()){ // Kontrollera om spelet är löst efter draget
             JOptionPane.showMessageDialog(null, "Grattis, du vann!");
         }
     }
